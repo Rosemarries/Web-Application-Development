8c62a828e91efb74c262dd5f558330b11978ac06
Rosemarries <rosemarries.404notfound@gmail.com>
2023-02-16 13:57:35 +0700

    ex04

--- JS_DOM/worker.js
+++ JS_DOM/worker.js
@@ -1,6 +1,20 @@
+let prev;
 function updateDate() {
     let currentTime = new Date().toString();
-    postMessage(currentTime);
+    let color1 = randomColor();
+    let color2 = randomColor();
+    while(color1 == color2 || prev == color1){
+        color1 = randomColor();
+    }
+    postMessage([currentTime, color1, color2]);
+    prev = color1;
     setTimeout(updateDate, 1000);
 }
 updateDate();
+
+function randomColor() {
+    const hex = ["red", "yellow", "green", "blue", "black", "white", "grey"];
+    let color;
+    color = hex[Math.floor(Math.random() * 6)];
+    return color;
+}
